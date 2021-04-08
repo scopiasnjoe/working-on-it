f458a39bbf603c0d59334d5a3aa64d9f5392f3d2
scopiasnjoe <82132647+scopiasnjoe@users.noreply.github.com>
2021-04-07 22:35:50 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "q4,@as{^Jsk-/sh!ySkJ":
+            case "Luigi":return img`
+. . . . 1 1 1 1 1 1 . . . . . . 
+. . . 1 1 1 1 1 1 1 1 1 1 1 . . 
+. . 7 7 7 7 d d d 7 d d . . . . 
+. 7 7 d 7 7 d d d 7 d d d . . . 
+. 7 7 d 7 7 d d d d 7 d d d . . 
+. 7 7 7 7 d d d d 7 7 7 7 . . . 
+. . . . d d d d d d d d d . . . 
+. 7 7 7 7 7 1 7 7 7 7 7 7 7 7 . 
+. 7 7 7 7 7 1 7 7 7 1 7 7 7 7 . 
+. 7 7 7 7 7 1 5 1 5 1 7 7 7 7 . 
+. 7 7 7 . 1 1 1 1 1 1 . 7 7 7 . 
+. d d d 1 1 1 1 1 1 1 1 d d d . 
+. d d d 1 1 1 1 1 1 1 1 d d d . 
+. d d 1 1 1 1 . . 1 1 1 1 d d . 
+. . 7 7 7 . . . . . . 7 7 7 . . 
+. 7 7 7 7 . . . . . . 7 7 7 7 . 
+`;
+            case "=|XaX]?UL6ImbkZ|G(0M":
+            case "fireball":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 2 . . . . . . . . 
+. . . . 2 . 2 2 . . . . . . . . 
+. . . . . 2 2 2 2 2 . . . . . . 
+. . 2 . . . 2 4 4 2 2 . . . . . 
+. . . . . . 2 4 4 4 2 . . . . . 
+. . . . 2 2 2 4 4 4 2 . . . . . 
+. . . 2 2 4 4 4 4 4 2 . . . . . 
+. . . 2 4 4 4 1 4 4 2 . . . . . 
+. . 2 4 4 4 1 4 4 4 2 . . . . . 
+. . 2 4 4 4 4 4 2 2 . . . . . . 
+. . 2 2 4 4 4 2 2 . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
